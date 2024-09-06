13f5da4cc7b747fb13621ce79a243591660ca75f
Pedro Paulo <pedro.paulo@agilean.com.br>
2024-09-06 20:54:49 -0300

    Fix: Update 3D Objects

--- src/services/circumferenceConnectService.py
+++ src/services/circumferenceConnectService.py
@@ -14,27 +14,10 @@ class CircumferenceConnectService:
             clY.append(df['y'][i])
             clZ.append(df['z'][i])
 
-            if h == 0:
-                clX.append(pc[0])
-                clY.append(pc[1])
-                clZ.append(pc[2])
-
-                clX.append(df['x'][i])
-                clY.append(df['y'][i])
-                clZ.append(df['z'][i])
-
         for i in range(len(df)):
             clX.append(df['x'][i])
             clY.append(df['y'][i])
             clZ.append(df['z'][i] + h)
-            if h == 2*raio:
-                clX.append(pc[0])
-                clY.append(pc[1])
-                clZ.append(pc[2] + h)
-
-                clX.append(df['x'][i])
-                clY.append(df['y'][i])
-                clZ.append(df['z'][i] + h)
 
         for i in range(len(df)):
             clX.append(df['x'][i])
