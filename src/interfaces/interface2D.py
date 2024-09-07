ad590af05b08e553e22f4eb0c87cbe4445c4dfe1
Pedro Paulo <pedro.paulo@agilean.com.br>
2024-09-07 20:58:37 -0300

    Feat: Add cam and projection

--- src/interfaces/interface2D.py
+++ src/interfaces/interface2D.py
@@ -1,1 +1,11 @@
-###TODO:Implement interface for 2D figures.
+import tkinter as tk
+from services.projectionService import ProjectionService
+
+class Interface2DFigures:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Interface 2D com Diversas Figuras")
+
+        # Botão para gerar a imagem
+        self.plot_button = tk.Button(root, text="Gerar Imagem", command=ProjectionService.generate_image)
+        self.plot_button.pack()
